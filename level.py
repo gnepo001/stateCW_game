090b879428746dcef547893e44000854b5d3c651
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-11-14 19:07:27 -0800

    added camera movement

--- level.py
+++ level.py
@@ -1,15 +1,40 @@
 import pygame
+from camera import CrosshairCamera
+from sprites import quickCreate
 
 class Level:
     def __init__(self):
+
+        #create sprite group to easily render/update/animate all sprites
+        self.all_sprites = CameraGroup()
         self.setup()
     
     def setup(self):
         self.display_surface = pygame.display.get_surface()
-        self.map=pygame.image.load("./assets/map.png").convert_alpha()
+        #load map
+        quickCreate(pos=(0,0),surf=pygame.image.load('./assets/map.png').convert_alpha(),groups=self.all_sprites)
+
+        #set player to camera 
+        self.player = CrosshairCamera((1000,1000),self.all_sprites)
     
     def run(self,dt):
         self.display_surface.fill("blue")
-        self.display_surface.blit(self.map,(0,0))
-        
+        #draw and update all sprites
+        self.all_sprites.custom_draw(self.player)
+        self.all_sprites.update(dt)
 
+#off set sprites to camera movement
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
+
+
+    def custom_draw(self,player):
+        self.offset.x = player.rect.centerx - 1920/2
+        self.offset.y = player.rect.centery - 1080/2
+        for sprite in self.sprites():
+            offset_rect = sprite.rect.copy()
+            offset_rect.center -= self.offset
+            self.display_surface.blit(sprite.image,offset_rect)
